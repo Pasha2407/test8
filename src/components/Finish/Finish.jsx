30e4eb023fc2cddc1cbd60d5a18576610689dc2c
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2025-03-05 14:22:56 +0100

    add finish

--- src/components/Finish/Finish.jsx
+++ src/components/Finish/Finish.jsx
@@ -1,4 +1,8 @@
 import { useEffect } from 'react';
+import gifCat from '../../images/f1.gif';
+import salut1 from '../../images/f2.gif';
+import salut2 from '../../images/f3.gif';
+import cat from '../../images/f4.png';
 import s from './Finish.module.css';
 
 export const Finish = () => {
@@ -16,7 +20,26 @@ export const Finish = () => {
 
   return (
     <div className={s.container}>
-      <p>Finish</p>
+      <div className={s.upS}>
+        <img src={salut2} alt="GIF" />
+      </div>
+      <div className={s.downS}>
+        <img src={salut1} alt="GIF" />
+        <img src={gifCat} alt="GIF" />
+        <img src={salut1} alt="GIF" />
+      </div>
+      <div className={s.text}>
+        <p>Вітаю з 8 березня</p>
+        <p>Сонечко я тебе дуже люблю</p>
+        <p>Ти в мене найкраща</p>
+        <div className={s.sup}>
+          <img src={cat} alt="IMG"></img>
+          <span>
+            Подаруночок появиться
+            <br /> зверху через пару секунд
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
